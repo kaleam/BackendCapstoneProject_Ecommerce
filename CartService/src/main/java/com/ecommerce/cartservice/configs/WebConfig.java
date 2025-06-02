e745800e6f327d8162deb16d9031a8f6d0416cef
Abhijeet Kale <mr.abhijeetkale@gmail.com>
2025-06-02 22:45:50 +0530

    added auth interceptor

--- CartService/src/main/java/com/ecommerce/cartservice/configs/WebConfig.java
+++ CartService/src/main/java/com/ecommerce/cartservice/configs/WebConfig.java
@@ -1,2 +1,19 @@
-package com.ecommerce.cartservice.configs;public class WebConfig {
+package com.ecommerce.cartservice.configs;
+
+import com.ecommerce.cartservice.controllers.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/cart/**");
+    }
 }
