5e430a7f72743f08c220d415a4b86e3201f459f8
Abhijeet Kale <mr.abhijeetkale@gmail.com>
2025-06-02 22:46:49 +0530

    added auth interceptor

--- PaymentService/src/main/java/com/ecommerce/paymentservice/controllers/AuthInterceptor.java
+++ PaymentService/src/main/java/com/ecommerce/paymentservice/controllers/AuthInterceptor.java
@@ -1,2 +1,57 @@
-package com.ecommerce.paymentservice.controllers;public class AuthInterceptor {
+package com.ecommerce.paymentservice.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+    private final RestTemplate restTemplate;
+
+    @Value("${user.service.url}")
+    private String tokenUrl;
+
+    public AuthInterceptor(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            try {
+                String token = authHeader.substring(7);
+
+                // Call user-service /validate-token endpoint
+                String validationUrl = tokenUrl;
+
+                // Set Authorization header and send validation request
+                HttpHeaders headers = new HttpHeaders();
+                headers.set("Authorization", "Bearer " + token);
+                HttpEntity<String> entity = new HttpEntity<>(headers);
+
+                ResponseEntity<String> validationResponse = restTemplate
+                        .exchange(validationUrl, HttpMethod.GET, entity, String.class);
+
+                if (validationResponse.getStatusCode().is2xxSuccessful()) {
+                    return true;
+                }
+            } catch (Exception e) {
+                // Log error if needed
+            }
+        }
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("Unauthorized");
+        return false;
+    }
 }
