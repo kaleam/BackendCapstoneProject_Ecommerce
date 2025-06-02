b9eb4acbb19ee31905d1befc7c145c34cdd7f24f
Abhijeet Kale <mr.abhijeetkale@gmail.com>
2025-06-02 22:46:15 +0530

    added auth interceptor

--- OrderManagementService/src/main/java/com/ecommerce/ordermanagementservice/configs/Config.java
+++ OrderManagementService/src/main/java/com/ecommerce/ordermanagementservice/configs/Config.java
@@ -1,9 +1,15 @@
 package com.ecommerce.ordermanagementservice.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableJpaAuditing
 public class Config {
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
