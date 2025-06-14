52271aa76c80ea0ebeea2e01ebc998a30e53addb
Abhijeet Kale <mr.abhijeetkale@gmail.com>
2025-06-15 02:30:05 +0530

    minor correction

--- CartService/src/main/java/com/ecommerce/cartservice/models/CartItem.java
+++ CartService/src/main/java/com/ecommerce/cartservice/models/CartItem.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class CartItem {
+public class CartItem extends BaseModel{
     private Long productId;
     private String productName;
     private int quantity;
